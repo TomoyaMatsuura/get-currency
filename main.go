b0761783b7f9f36edf9f783e76afb33f9d82c1ad
niwa <naoki_niwa@mj.makita.co.jp>
2023-05-24 16:55:25 +0900

    err mod

--- main.go
+++ main.go
@@ -20,10 +20,10 @@ type Rates struct {
 }
 
 type JsonType struct {
-	success   bool   //true
-	timestamp int    //1684897743
-	base      string //"EUR"
-	date      string //"2023-05-24"
+	Success   bool   //true
+	Timestamp int    //1684897743
+	Base      string //"EUR"
+	Date      string //"2023-05-24"
 	Rates     Rates  //{"USD":1.078411,"JPY":149.318947,"BRL":5.362724,"MXN":19.361185,"ARS":253.343999,"CLP":863.806891,"PEN":3.987355,"BOB":7.454886}
 }
 
